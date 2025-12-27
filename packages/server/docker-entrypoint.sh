ab4472dd8f8560c0135caf7e5d55b95fb8e1db59
shevtsod <daniel.shevtsov@gmail.com>
2025-12-26 18:09:40 -0600

    feat(server): Add authentication (#6)
    - Fix Dockerfiles in monorepo
    - Update NPM deps

--- packages/server/docker-entrypoint.sh
+++ packages/server/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -euo pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 # Set environment variable defaults based on .env.example
 NODE_ENV="${NODE_ENV:-development}"
 
@@ -11,10 +13,10 @@ CACHE_HOST="${CACHE_HOST:-127.0.0.1}"
 CACHE_PORT="${CACHE_PORT:-6379}"
 
 echo "Waiting for database at \"$DB_HOST:$DB_PORT\" ..."
-./wait-for "$DB_HOST:$DB_PORT"
+"${SCRIPT_DIR}/wait-for" "$DB_HOST:$DB_PORT"
 
 echo "Waiting for cache at \"$CACHE_HOST:$CACHE_PORT\" ..."
-./wait-for "$CACHE_HOST:$CACHE_PORT"
+"${SCRIPT_DIR}/wait-for" "$CACHE_HOST:$CACHE_PORT"
 
 npm run cli db migrate
 npm run cli db seed

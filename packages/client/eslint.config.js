9ca52eed1973c24337bfa12d9496443cac8db7f3
shevtsod <daniel.shevtsov@gmail.com>
2025-12-26 18:09:40 -0600

    feat(server): Add authentication (#6)
    - Fix Dockerfiles in monorepo
    - Update NPM deps

--- packages/client/eslint.config.js
+++ packages/client/eslint.config.js
@@ -1,23 +1,26 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import eslintPluginJestDom from "eslint-plugin-jest-dom";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
+      // https://github.com/testing-library/eslint-plugin-jest-dom
+      eslintPluginJestDom.configs["flat/recommended"],
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
-])
+]);
